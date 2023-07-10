a5b6a0b26f4eab0d14bf97433e60437b1a8c8c0e
wolf <billodida420@gmail.com>
2023-07-11 00:06:31 +0300

    setting up the app

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
+  mainWindow.loadURL('https://agent-install-bkz6.vercel.app/');
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
